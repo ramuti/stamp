50b7ff1505d00c87420256e33510625ed52621de
ramuti <73601694+ramuti@users.noreply.github.com>
2025-08-27 10:07:42 +0900

    update.js を作成

---
+++ update.js
@@ -0,0 +1,16 @@
+{
+  "cards": [
+    {
+      "id": "card_1756201790352",
+      "name": "かす",
+      "slots": 5,
+      "addPass": "なす",
+      "bg": "#fff0f5",
+      "stampIcon": "",
+      "notifyMsg": "",
+      "maxNotifyMsg": ""
+    }
+  ],
+  "keywords": [],
+  "updates": []
+}
