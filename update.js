ef20a9b5698072fbb5a19b96f89dd7b38366e075
ramuti <73601694+ramuti@users.noreply.github.com>
2025-08-29 01:23:05 +0900

    update.js を更新

--- update.js
+++ update.js
@@ -1,54 +1,7 @@
 // update.js
 var update = {
-  "cards": [
-    {
-      "id": "card_1756396165884",
-      "name": "カード",
-      "slots": 5,
-      "addPass": "あかさ",
-      "bg": "#fff0f5",
-      "stampIcon": "",
-      "notifyMsg": "",
-      "maxNotifyMsg": ""
-    },
-    {
-      "id": "card_1756396762813",
-      "name": "さしす",
-      "slots": 5,
-      "addPass": "すす",
-      "bg": "#fff0f5",
-      "stampIcon": "",
-      "notifyMsg": "",
-      "maxNotifyMsg": ""
-    },
-    {
-      "id": "card_1756396788158",
-      "name": "かきく",
-      "slots": 5,
-      "addPass": "ろろ",
-      "bg": "#fff0f5",
-      "stampIcon": "",
-      "notifyMsg": "",
-      "maxNotifyMsg": ""
-    },
-    {
-      "id": "card_1756396795322",
-      "name": "ままま",
-      "slots": 5,
-      "addPass": "しし",
-      "bg": "#fff0f5",
-      "stampIcon": "",
-      "notifyMsg": "",
-      "maxNotifyMsg": ""
-    }
-  ],
-  "keywords": [
-    {
-      "cardId": "card_1756396165884",
-      "word": "まみむ",
-      "enabled": true
-    }
-  ],
+  "cards": [],
+  "keywords": [],
   "updates": [
     {
       "date": "2025/8/29 0:43:54",
