b4197b3e1c99c973dcf4218c117be7009c76ef6c
ramuti <73601694+ramuti@users.noreply.github.com>
2025-08-29 00:49:56 +0900

    update.js を更新

--- update.js
+++ update.js
@@ -2,10 +2,10 @@
 var update = {
   "cards": [
     {
-      "id": "card_1756392210412",
-      "name": "あいう",
+      "id": "card_1756396165884",
+      "name": "カード",
       "slots": 5,
-      "addPass": "むい",
+      "addPass": "あかさ",
       "bg": "#fff0f5",
       "stampIcon": "",
       "notifyMsg": "",
@@ -14,19 +14,19 @@ var update = {
   ],
   "keywords": [
     {
-      "cardId": "card_1756392210412",
-      "word": "ああ",
+      "cardId": "card_1756396165884",
+      "word": "まみむ",
       "enabled": true
     }
   ],
   "updates": [
     {
-      "date": "2025/8/27 18:53:49",
-      "msg": "あ"
-    },
-    {
       "date": "2025/8/29 0:43:54",
       "msg": "まみむ"
+    },
+    {
+      "date": "2025/8/29 0:49:35",
+      "msg": "るる"
     }
   ]
 };
